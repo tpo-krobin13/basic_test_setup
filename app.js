e64edd9a4ddbec308508676591e6dcf0dc9d2e33
tpo-krobin13 <krobin13@travelers.com>
2021-02-19 09:53:19 -0500

    initial commits

---
+++ app.js
@@ -0,0 +1,76 @@
+// create weather application
+// user types in zip code from command line and it retrieves a weather message from remote service
+  // api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}
+  //  api key: 70a390896e3dd463d387be85156412d3
+  // url 
+
+
+
+// ensure you can retrieve params from command line +
+// ensure only digits are provided +
+// ensure incorrect entries generate an error +
+// call remote api and pass zip code
+// ensure remote api returns a response
+// ensure remote url problems are handled with an error
+// ensure remote parsed to valid json
+// ensure remote json has the values
+// print message back to the screen
+
+var https = require('https');
+var fetch = require('node-fetch');
+
+
+function getArguments(inString) {
+  return inString.slice(2);
+}
+
+function validateInput(inValue){
+  const regexp = /^\d{5}$/;
+  if (inValue !== null && regexp.test(inValue) && +inValue > 0) {
+    return true;
+  } else {
+    return new Error('Invalid user input.');
+  }
+
+}
+function getRemoteUrl (zipCode) {
+    if(validateInput(zipCode)){
+      return `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=70a390896e3dd463d387be85156412d3`;
+    } else {
+      throw (new Error('Unable to process request'));
+    }
+}
+
+async function getOpenAPIWeather (zipCode , callback) {
+  const url = getRemoteUrl(zipCode);
+  return await fetch(url)
+  .then((response) => response.json())
+  .then(data => callback(data))
+  .catch(error => printMessage(new Error('Unable to process request')));
+}
+
+function convertKelvenToFahrenheit (value) {
+  return Math.ceil((parseFloat(value) * 1.8) -459.67);
+}
+
+function printMessage(jsonObject) {
+  let tempAsFloat = parseFloat()
+  const msg = `The weather in ${jsonObject.name} is ${jsonObject.weather[0].description} and the temperature is ${convertKelvenToFahrenheit(jsonObject.main.temp)} degrees`;
+  return msg;
+}
+
+
+initApp();
+function initApp () {
+  getArguments(process.argv).forEach((zip) => {
+    getOpenAPIWeather(zip, printMessage);
+  });
+}
+
+
+module.exports.getArguments = getArguments;
+module.exports.validateInput = validateInput;
+module.exports.getRemoteUrl = getRemoteUrl;
+module.exports.convertKelvenToFahrenheit = convertKelvenToFahrenheit;
+module.exports.printMessage = printMessage;
+module.exports.getOpenAPIWeather = getOpenAPIWeather;
---
+++ test/main_test.js
@@ -0,0 +1,115 @@
+var expect = require('chai').expect;
+var actual ;
+var expected;
+
+describe('Mocha', function () {
+  // Test spec (unit test)
+
+  it('should run our tests using npm', function () {
+    expect(true).to.be.ok;
+  })
+})
+
+  describe('App.js', function () {
+  describe('getArguments', function () {
+      var getArguments = require('../app.js').getArguments;
+      it('should remove first two commandLine arguments', function () {
+        const argumentString = ['/opt/homebrew/Cellar/node/15.8.0/bin/node','/Users/krobinson/Development/vsc/th_profile_app/app.js','kenny','otherUser'];
+
+          expected = ['kenny','otherUser'];
+          actual = getArguments(argumentString);
+
+          expect(actual).to.deep.equal(expected);
+      })
+  })
+  describe('validateInput', function () {
+    var validateInput = require('../app.js').validateInput;
+    it('should throw an error when letters are present', function () {
+      
+      expected = 'error';
+      actual = validateInput('ggggg');
+      expect(actual).to.be.an('error');
+
+      expected = 'error';
+      actual = validateInput('0000w');
+      expect(actual).to.be.an('error');
+    })
+    it('should throw an error when the incorrect number of digits is present', function () {
+      
+      expected = 'error';
+      actual = validateInput('000');
+      expect(actual).to.be.an('error');
+
+      expected = 'error';
+      actual = validateInput('000000');
+      expect(actual).to.be.an('error');
+    })
+
+
+    it('should return true when five digits are provided', function () {
+      
+      expected = 'error';
+      actual = validateInput('30331');
+      expect(actual).to.be.true;
+
+      expected = 'error';
+      actual = validateInput('98736');
+      expect(actual).to.be.true;
+    })
+    it('should throw an error when the value is less than 00001', function () {
+      
+      expected = 'error';
+      actual = validateInput('00000');
+      expect(actual).to.be.an('error');
+    })
+
+
+  })
+  describe('GetRemoteUrl', function () {
+    var getRemoteUrl = require('../app.js').getRemoteUrl;
+    it('should return a valid url string', function () {
+      expected = 'http://api.openweathermap.org/data/2.5/weather?zip=30331,us&appid=70a390896e3dd463d387be85156412d3';
+      actual  = getRemoteUrl(30331);
+    
+      expect(actual).to.equal(expected);
+    })
+  })
+  describe('convertKelvenToFahrenheit', function () {
+    var convertKelvenToFahrenheit = require('../app.js').convertKelvenToFahrenheit;
+    it('should properly convert Kelvin to Fahrenheit', function () {
+
+      expected = 47;
+      actual = convertKelvenToFahrenheit(281.47);
+      expect(actual).to.equal(expected);
+
+      expected = 81;
+      actual = convertKelvenToFahrenheit(300);
+      expect(actual).to.equal(expected);
+    })
+  })
+
+  describe('printMessage', function () {
+    var printMessage = require('../app.js').printMessage;
+    var jsonData = JSON.parse('{"coord":{"lon":-84.5205,"lat":33.7224},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],"base":"stations","main":{"temp":281.47,"feels_like":278.23,"temp_min":280.93,"temp_max":282.15,"pressure":1016,"humidity":81},"visibility":10000,"wind":{"speed":3.09,"deg":40},"clouds":{"all":90},"dt":1613339034,"sys":{"type":1,"id":4155,"country":"US","sunrise":1613305398,"sunset":1613344885},"timezone":-18000,"id":0,"name":"Atlanta","cod":200}');
+
+    it('should print the users weather string', function (done) {
+      expected = 'The weather in Atlanta is overcast clouds and the temperature is 47 degrees';
+      actual = printMessage(jsonData);
+
+      expect(actual).to.equal(expected);
+      done();
+    })
+  })
+
+  describe('getOpenAPIWeather', function () {
+    var printMessage = require('../app.js').printMessage;
+    var getOpenAPIWeather = require('../app.js').getOpenAPIWeather;
+    var jsonData = JSON.parse('{"coord":{"lon":-84.5205,"lat":33.7224},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],"base":"stations","main":{"temp":281.47,"feels_like":278.23,"temp_min":280.93,"temp_max":282.15,"pressure":1016,"humidity":81},"visibility":10000,"wind":{"speed":3.09,"deg":40},"clouds":{"all":90},"dt":1613339034,"sys":{"type":1,"id":4155,"country":"US","sunrise":1613305398,"sunset":1613344885},"timezone":-18000,"id":0,"name":"Atlanta","cod":200}');
+
+    it('should print the users weather string', function (done) {
+      actual = getOpenAPIWeather(30331, printMessage);
+      done();
+      expect(actual).is.not.empty;
+    })
+  })
+});
